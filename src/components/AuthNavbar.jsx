9b4202d76b47939c296a75d3ee8f65a1464caa11
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2024-11-02 18:40:20 +0530

    implemented google auth for login

--- src/components/AuthNavbar.jsx
+++ src/components/AuthNavbar.jsx
@@ -1,8 +1,19 @@
+import { useGoogleLogin } from "@react-oauth/google";
+
 const AuthNavbar = () => {
+
+    const googleLogin = useGoogleLogin({
+        onSuccess: (response) => {
+            console.log("login succ",response)
+        },
+        onError: () => {
+            console.log('error')
+        }
+    })
     return (
         <div className=" w-full flex justify-between items-center p-1 lg:p-2">
             <h1>Wicket<span className="text text-blue-600">Cast</span></h1>
-            <button className="bg-blue-600 text-white text-xs p-2 rounded hover:text-blue-600 hover:bg-white hover:border border-blue-600">Get Started</button>
+            <button onClick={()=> googleLogin()} className="bg-blue-600 text-white text-xs p-2 rounded hover:text-blue-600 hover:bg-white hover:border border-blue-600">Get Started</button>
        </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
 
 createRoot(document.getElementById('root')).render(
+  <GoogleOAuthProvider clientId={googleClientId}>
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+    </StrictMode>
+    </GoogleOAuthProvider>,
 )
