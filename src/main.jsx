e77eb9f47caf8582bbe5a3f1144c93c0b80f432d
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2024-11-10 11:59:04 +0530

    implemented vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,12 @@ import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import Weather from "./modals/Weather.jsx";
+import { Analytics } from "@vercel/analytics/react"
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Analytics>
     <BrowserRouter>
       <GoogleOAuthProvider clientId={googleClientId}>
         <AuthProvider>
@@ -17,6 +19,7 @@ createRoot(document.getElementById("root")).render(
           {/* <Weather/> */}
         </AuthProvider>
       </GoogleOAuthProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+      </Analytics>
   </StrictMode>
 );
